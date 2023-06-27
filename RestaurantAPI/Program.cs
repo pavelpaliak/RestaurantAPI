00d44b53ed4731649a727e84af439a5313b7bb4c
Pavel Paliak <lastykron@gmail.com>
2023-06-27 15:28:44 +0200

    Tworzenie bazy danych mssql.

--- RestaurantAPI/Program.cs
+++ RestaurantAPI/Program.cs
@@ -7,11 +7,15 @@ builder.Services.AddControllers();
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseDeveloperExceptionPage();
+}
 
 app.UseHttpsRedirection();
-
-app.UseAuthorization();
-
-app.MapControllers();
-
+app.UseRouting();
+app.UseEndpoints(endpoints =>
+{
+    app.MapControllers();
+});
 app.Run();
