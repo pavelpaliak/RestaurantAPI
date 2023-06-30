0d798829ee301ecf420d7387caab4450974114ce
Pavel Paliak <lastykron@gmail.com>
2023-06-30 09:14:03 +0200

    Tworzenie encji

--- RestaurantAPI/Controllers/RestaurantController.cs
+++ RestaurantAPI/Controllers/RestaurantController.cs
@@ -17,6 +17,17 @@ namespace RestaurantAPI.Controllers
             _dbContext = dbContext;
             _mapper = mapper;
         }
+
+        [HttpPost]
+        public ActionResult CreateRestaurant([FromBody] CreateRestaurantDto dto)
+        {
+            var restaurant = _mapper.Map<Restaurant>(dto);
+            _dbContext.Restaurants.Add(restaurant);
+            _dbContext.SaveChanges();
+
+            return Created($"/api/restaurant/{restaurant.Id}", null);
+        }
+
         [HttpGet]
         public ActionResult<IEnumerable<RestaurantDto>> GetAll()
         {
