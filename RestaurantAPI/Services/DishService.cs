cd702e8bef09d7e349e8fb6379969de755a135e3
Pavel Paliak <lastykron@gmail.com>
2023-07-07 11:55:35 +0200

    Pobieranie subencji

--- RestaurantAPI/Services/DishService.cs
+++ RestaurantAPI/Services/DishService.cs
@@ -1,4 +1,5 @@
 ﻿using AutoMapper;
+using Microsoft.EntityFrameworkCore;
 using RestaurantAPI.Entities;
 using RestaurantAPI.Exceptions;
 using RestaurantAPI.Models;
@@ -8,6 +9,8 @@ namespace RestaurantAPI.Services
     public interface IDishService
     {
         int Create(int restaurantId, CreateDishDto dto);
+        DishDto GetById(int restaurantId, int dishId);
+        List<DishDto> GetAll(int restaurantId);
     }
     public class DishService : IDishService
     {
@@ -33,5 +36,37 @@ namespace RestaurantAPI.Services
 
             return dishEntity.Id;
         }
+        public DishDto GetById(int restaurantId, int dishId)
+        {
+            var restaurant = _context.Restaurants.FirstOrDefault(r => r.Id == restaurantId);
+            if (restaurant is null)
+                throw new NotFoundException("Restaurant not found");
+
+            var dish = _context.Dishes.FirstOrDefault(d => d.Id == dishId);
+            if(dish is null || dish.RestaurantId != restaurantId)
+            {
+                throw new NotFoundException("Dish not found");
+            }
+
+            var dishDto = _mapper.Map<DishDto>(dish);
+            return dishDto;
+        }
+
+        public List<DishDto> GetAll(int restaurantId) 
+        {
+            var restaurant = _context
+                .Restaurants
+                .Include(r => r.Dishes)
+                .FirstOrDefault(r => r.Id == restaurantId);
+
+            if (restaurant is null)
+                throw new NotFoundException("Restaurant not found");
+
+            var dishDtos = _mapper.Map<List<DishDto>>(restaurant.Dishes);
+
+            return dishDtos;
+        }
+
+    
     }
 }
