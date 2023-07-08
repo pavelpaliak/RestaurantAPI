68dae18f4b705ac5cbe72edf2ababeb08958f410
Pavel Paliak <lastykron@gmail.com>
2023-07-08 08:21:26 +0200

    Autentykacja. Tabeli z rolami oraz użytkownikami.

--- RestaurantAPI/RestaurantSeeder.cs
+++ RestaurantAPI/RestaurantSeeder.cs
@@ -15,6 +15,13 @@ namespace RestaurantAPI
         {
             if(_dbContext.Database.CanConnect())
             {
+                if (!_dbContext.Roles.Any())
+                {
+                    var roles = GetRoles();
+                    _dbContext.Roles.AddRange(roles);
+                    _dbContext.SaveChanges();
+                }
+
                 if(!_dbContext.Restaurants.Any())
                 {
                     var restaurants = GetRestaurants();
@@ -22,6 +29,25 @@ namespace RestaurantAPI
                     _dbContext.SaveChanges();
                 }
             }
+        }
+        private IEnumerable<Role> GetRoles()
+        {
+            var roles = new List<Role>()
+            {
+                new Role()
+                {
+                    Name = "User"
+                },
+                new Role()
+                {
+                    Name = "Manager"
+                },
+                new Role()
+                {
+                    Name = "Admin"
+                }
+            };
+            return roles;
         }
 
         private IEnumerable<Restaurant> GetRestaurants()
