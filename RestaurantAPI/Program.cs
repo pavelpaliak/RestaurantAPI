4d0227e8a0c12e7e9c9f045defe348dfb84b7993
Pavel Paliak <lastykron@gmail.com>
2023-07-09 10:52:26 +0200

    Logowanie Użytkownika.

--- RestaurantAPI/Program.cs
+++ RestaurantAPI/Program.cs
@@ -9,10 +9,33 @@ using FluentValidation;
 using RestaurantAPI.Models;
 using RestaurantAPI.Validators;
 using FluentValidation.AspNetCore;
+using Microsoft.IdentityModel.Tokens;
+using System.Text;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+var authenticationSettings = new AuthenticationSettings();
+
+builder.Configuration.GetSection("Authentication").Bind(authenticationSettings);
+builder.Services.AddSingleton(authenticationSettings);
+builder.Services.AddAuthentication(option =>
+{
+    option.DefaultAuthenticateScheme = "Bearer";
+    option.DefaultScheme = "Bearer";
+    option.DefaultChallengeScheme = "Bearer";
+}).AddJwtBearer(cfg =>
+{
+    cfg.RequireHttpsMetadata = false;
+    cfg.SaveToken = true;
+    cfg.TokenValidationParameters = new TokenValidationParameters
+    {
+        ValidIssuer = authenticationSettings.JwtIssure,
+        ValidAudience = authenticationSettings.JwtIssure,
+        IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(authenticationSettings.JwtKey)),
+    };
+});
+
 builder.Logging.ClearProviders();
 builder.Host.UseNLog();
 builder.Services.AddControllers().AddFluentValidation();
@@ -40,6 +63,7 @@ if (app.Environment.IsDevelopment())
 }
 app.UseMiddleware<ErrorHandlingMiddleware>();
 app.UseMiddleware<RequestTimeMiddleware>();
+app.UseAuthentication();
 app.UseHttpsRedirection();
 
 app.UseSwagger();
--- RestaurantAPI/Services/AccountService.cs
+++ RestaurantAPI/Services/AccountService.cs
@@ -1,23 +1,34 @@
 ﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.IdentityModel.Tokens;
 using RestaurantAPI.Entities;
+using RestaurantAPI.Exceptions;
 using RestaurantAPI.Models;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
 
 namespace RestaurantAPI.Services
 {
     public interface IAccountService
     {
+        string GenerateJwt(LoginDto dto);
         void RegisterUser(RegisterUserDto dto);
     }
     public class AccountService : IAccountService
     {
         private readonly RestaurantDBContext _context;
         private readonly IPasswordHasher<User> _passwordHasher;
+        private readonly AuthenticationSettings _authenticationSettings;
 
-        public AccountService(RestaurantDBContext context, IPasswordHasher<User> passwordHasher)
+        public AccountService(RestaurantDBContext context, IPasswordHasher<User> passwordHasher, AuthenticationSettings authenticationSettings)
         {
             _context = context;
             _passwordHasher = passwordHasher;
+            _authenticationSettings = authenticationSettings;
         }
+
+
         public void RegisterUser(RegisterUserDto dto)
         {
 
@@ -33,6 +44,46 @@ namespace RestaurantAPI.Services
             newUser.PasswordHash = hashedPassword;
             _context.Users.Add(newUser);
             _context.SaveChanges();
+        }
+
+        public string GenerateJwt(LoginDto dto)
+        {
+            var user = _context.Users
+                .Include(u => u.Role)
+                .FirstOrDefault(u => u.Email == dto.Email);
+
+            if (user is null) 
+            {
+                throw new BadRequestException("Invalid user name or password");
+            }
+
+            var result = _passwordHasher.VerifyHashedPassword(user, user.PasswordHash, dto.Password);
+            if(result == PasswordVerificationResult.Failed)
+            {
+                throw new BadRequestException("Invalid user name or password");
+            }
+
+            var claims = new List<Claim>()
+            {
+                new Claim(ClaimTypes.NameIdentifier, user.Id.ToString()),
+                new Claim(ClaimTypes.Name, $"{user.FirstName} {user.LastName}"),
+                new Claim(ClaimTypes.Role, $"{user.Role}"),
+                new Claim("DateOfBirth", user.DateOfBirth.Value.ToString("yyyy-MM-dd")),
+                new Claim("Nationality", user.Nationality)
+            };
+
+            var key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(_authenticationSettings.JwtKey));
+            var cred = new SigningCredentials(key, SecurityAlgorithms.HmacSha256);
+            var expires = DateTime.Now.AddDays(_authenticationSettings.JwtExpireDays);
+
+            var token = new JwtSecurityToken(_authenticationSettings.JwtIssure,
+                _authenticationSettings.JwtIssure,
+                claims,
+                expires: expires,
+                signingCredentials: cred);
+
+            var tokenHandler = new JwtSecurityTokenHandler();
+            return tokenHandler.WriteToken(token);
         }
     }
 }
